84d0aec5e9a3a3bdf69c10dbcdd9afa24f8e27ef
fengyafei <fengyafei@odianyun.com>
2020-05-03 15:39:40 +0800

    master

---
+++ src/main/java/com/example/algorithm/AlgorithmTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmTestApplication.class, args);
+	}
+
+}
